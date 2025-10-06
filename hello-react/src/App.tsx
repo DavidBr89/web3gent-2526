72b2c919a4ea4bc989a9c013064817ac0b68b26e
David Breckx <david.breckx@telenet.be>
2025-10-06 17:30:03 +0200

    Einde les 3

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import Test from "./components/Test";
+import Header from "./components/Header";
+
+type ID = number | string;
+type WebType = "Web 1" | "Web 2" | "Web 3";
+
+type Person = {
+  fName: string;
+  lName: string;
+  age: number;
+  hasCar?: boolean;
+};
+
+type Student = Person & {
+  studentNumber: string;
+};
+
+interface IStudent extends Person {
+  studentNumber: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  let title: WebType = "Web 3";
+  const id: ID = 3;
+
+  const student: IStudent = {
+    fName: "John",
+    lName: "Doe",
+    age: 23,
+    studentNumber: "dr242r523r234ff",
+  };
+
+  title = "Web 2";
+
+  const students: Array<Student> = [];
+
+  students.push(5);
+
+  console.log(title);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ hello-react/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Web 3</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum odio
+        sunt numquam vero, tenetur impedit repellat eaque error quis,
+        consequatur incidunt quisquam ipsa eius iusto reiciendis, vel est
+        placeat natus?
+      </p>
+    </div>
+  );
+};
+
+export default Header;
